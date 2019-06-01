9eec9cd059dc77f509ec365f6188d9ddcb63313d
arsenmk <arsenmk@amazon.com>
2019-05-31 18:30:52 -0700

    Migrating SqlParserTests, TermQueryExplainTest, and UtilsTest to run in gradle build.
    Also updated the rest handler to treat VerificationException as client
    side issue.
    
    How tested: Successful gradle build

--- src/main/java/com/amazon/opendistroforelasticsearch/sql/plugin/RestSqlAction.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/sql/plugin/RestSqlAction.java
@@ -25,6 +25,7 @@ import com.amazon.opendistroforelasticsearch.sql.metrics.Metrics;
 import com.amazon.opendistroforelasticsearch.sql.query.QueryAction;
 import com.amazon.opendistroforelasticsearch.sql.request.SqlRequest;
 import com.amazon.opendistroforelasticsearch.sql.request.SqlRequestFactory;
+import com.amazon.opendistroforelasticsearch.sql.rewriter.matchtoterm.VerificationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.node.NodeClient;
@@ -118,7 +119,8 @@ public class RestSqlAction extends BaseRestHandler {
                e instanceof ParserException ||
                e instanceof SQLFeatureNotSupportedException ||
                e instanceof IllegalArgumentException ||
-               e instanceof IndexNotFoundException;
+               e instanceof IndexNotFoundException ||
+               e instanceof VerificationException;
     }
 
     private RestChannelConsumer reportError(Exception e, RestStatus status) {
