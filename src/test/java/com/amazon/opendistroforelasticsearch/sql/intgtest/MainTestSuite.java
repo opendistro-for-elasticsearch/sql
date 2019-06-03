f6c9eaf14a2b15a6657bd0d34c66978d378fc3e4
Peng Huo <penghuo@amazon.com>
2019-06-03 13:37:02 -0700

    Migrate JSONRequestIT, MathFunctionsIT, MethodQueryIT to esintgtest package (#66)
    * Migrate JSONRequestIT, MathFunctionsIT, MethodQueryIT to esintgtest package

--- src/test/java/com/amazon/opendistroforelasticsearch/sql/intgtest/MainTestSuite.java
+++ src/test/java/com/amazon/opendistroforelasticsearch/sql/intgtest/MainTestSuite.java
@@ -66,7 +66,6 @@ import static org.mockito.Mockito.when;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     QueryTest.class,
-    MethodQueryTest.class,
     AggregationTest.class,
     HashJoinBasicTest.class,
     HashJoinMoreTest.class,
@@ -75,9 +74,7 @@ import static org.mockito.Mockito.when;
     MultiQueryTests.class,
     DeleteTest.class,
     HavingTest.class,
-    JSONRequestTest.class,
     NestedFieldQueryTest.class,
-    MathFunctionsTest.class,
     DateFunctionsTest.class,
     QueryFunctionsTest.class,
     PrettyFormatResponseTest.class,
