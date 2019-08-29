e059c76bc67a4c59ce3eff4c2f1297f9ce61436b
Hussain <abbashus@amazon.com>
2019-08-29 11:10:33 -0700

    Added integ test for exception

--- src/test/java/com/amazon/opendistroforelasticsearch/sql/esintgtest/NestedFieldQueryIT.java
+++ src/test/java/com/amazon/opendistroforelasticsearch/sql/esintgtest/NestedFieldQueryIT.java
@@ -16,11 +16,13 @@
 package com.amazon.opendistroforelasticsearch.sql.esintgtest;
 
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.ResponseException;
 import org.elasticsearch.common.xcontent.LoggingDeprecationHandler;
 import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
@@ -287,6 +289,23 @@ public class NestedFieldQueryIT extends SQLIntegTestCase {
 
         JSONObject results = executeQuery(sql);
         Assert.assertThat(getTotalHits(results), equalTo(4));
+    }
+
+    @Test
+    public void leftJoinExceptionOnExtraNestedFields() throws IOException {
+        String sql = "SELECT * " +
+                     "FROM elasticsearch-sql_test_index_employee_nested e " +
+                     "LEFT JOIN e.projects p, e.comments c ";
+
+        try {
+            String explain = explainQuery(sql);
+            Assert.fail("Expected ResponseException, but none was thrown");
+        } catch (ResponseException e) {
+            assertThat(e.getResponse().getStatusLine().getStatusCode(), equalTo(RestStatus.BAD_REQUEST.getStatus()));
+            final String entity = TestUtils.getResponseBody(e.getResponse());
+            assertThat(entity, containsString("only single nested field is allowed as right table for LEFT JOIN"));
+            assertThat(entity, containsString("\"type\":\"verification_exception\""));
+        }
     }
 
 
