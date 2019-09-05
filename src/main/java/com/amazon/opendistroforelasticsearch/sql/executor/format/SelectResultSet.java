afac65897b933aada040928c03507221a28e4032
Andy Galkin <galk@amazon.com>
2019-09-05 15:26:35 -0700

    removed unnecessary change, fixes #128

--- src/main/java/com/amazon/opendistroforelasticsearch/sql/executor/format/SelectResultSet.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/sql/executor/format/SelectResultSet.java
@@ -317,7 +317,7 @@ public class SelectResultSet extends ResultSet {
                 // (similar to Strategy pattern)
 
                 return SQLFunctions.getScriptFunctionReturnType(
-                        ((ScriptMethodField) field).getFunctionName().toLowerCase());
+                        ((ScriptMethodField) field).getFunctionName());
             }
             default:
                 throw new UnsupportedOperationException(
