f6e277d747d1248f443e961b0c6270747eb5f89d
Peng <penghuo@amazon.com>
2019-09-10 15:51:39 -0700

    Add parent for SQLExpr in AST if missing

--- src/main/java/com/amazon/opendistroforelasticsearch/sql/rewriter/nestedfield/NestedFieldRewriter.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/sql/rewriter/nestedfield/NestedFieldRewriter.java
@@ -17,7 +17,6 @@ package com.amazon.opendistroforelasticsearch.sql.rewriter.nestedfield;
 
 import com.alibaba.druid.sql.ast.expr.SQLBinaryOpExpr;
 import com.alibaba.druid.sql.ast.expr.SQLIdentifierExpr;
-import com.alibaba.druid.sql.ast.expr.SQLInSubQueryExpr;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlSelectQueryBlock;
 import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlASTVisitorAdapter;
 
@@ -82,15 +81,6 @@ public class NestedFieldRewriter extends MySqlASTVisitorAdapter {
         if (curScope().isAnyNestedField() && isNotGroupBy(query)) {
             new Select(query.getSelectList()).rewrite(curScope());
         }
-        return true;
-    }
-
-    /**
-     * Fix null parent problem which is required by SQLIdentifier.visit()
-     */
-    @Override
-    public boolean visit(SQLInSubQueryExpr subQuery) {
-        subQuery.getExpr().setParent(subQuery);
         return true;
     }
 
