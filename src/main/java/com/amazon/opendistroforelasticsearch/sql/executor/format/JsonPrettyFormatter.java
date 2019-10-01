8b7e44a2b07a712c65337f5cd25c419b01f30758
chloe-zh <fizhang@amazon>
2019-09-30 22:10:59 -0700

    Add a formatter to print explain response in pretty format of json.

---
+++ src/main/java/com/amazon/opendistroforelasticsearch/sql/executor/format/JsonPrettyFormatter.java
@@ -0,0 +1,39 @@
+/*
+ *   Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.sql.executor.format;
+
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.xcontent.*;
+
+import java.io.IOException;
+
+public class JsonPrettyFormatter {
+
+    public JsonPrettyFormatter() {
+    }
+
+    public String formatter(String jsonString) throws IOException {
+        //turn _explain response into pretty formatted Json
+        XContentBuilder contentBuilder = XContentFactory.jsonBuilder().prettyPrint();
+        try (
+                XContentParser contentParser = XContentFactory.xContent(XContentType.JSON)
+                        .createParser(NamedXContentRegistry.EMPTY, LoggingDeprecationHandler.INSTANCE, jsonString)
+        ){
+            contentBuilder.copyCurrentStructure(contentParser);
+        }
+        return Strings.toString(contentBuilder);
+    }
+}
