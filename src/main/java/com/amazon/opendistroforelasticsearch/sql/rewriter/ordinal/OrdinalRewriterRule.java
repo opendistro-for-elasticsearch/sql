065064f20c6efad91adbe93b980025ae393da23a
Hussain <abbashus@amazon.com>
2019-10-23 15:36:40 -0700

    Address some comments

--- src/main/java/com/amazon/opendistroforelasticsearch/sql/rewriter/ordinal/OrdinalRewriterRule.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/sql/rewriter/ordinal/OrdinalRewriterRule.java
@@ -26,9 +26,7 @@ import com.alibaba.druid.sql.ast.statement.SQLSelectQuery;
 import com.alibaba.druid.sql.dialect.mysql.ast.expr.MySqlSelectGroupByExpr;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlSelectQueryBlock;
 import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlASTVisitorAdapter;
-import com.alibaba.druid.sql.parser.ParserException;
 import com.alibaba.druid.sql.parser.SQLExprParser;
-import com.alibaba.druid.sql.parser.Token;
 import com.amazon.opendistroforelasticsearch.sql.parser.ElasticSqlExprParser;
 import com.amazon.opendistroforelasticsearch.sql.rewriter.RewriteRule;
 import com.amazon.opendistroforelasticsearch.sql.rewriter.RewriteRuleExecutor;
@@ -57,7 +55,7 @@ public class OrdinalRewriterRule implements RewriteRule<SQLQueryExpr> {
     }
 
     @Override
-    public boolean match(SQLQueryExpr root) throws SQLFeatureNotSupportedException {
+    public boolean match(SQLQueryExpr root) {
         SQLSelectQuery sqlSelectQuery = root.getSubQuery().getQuery();
          if (!(sqlSelectQuery instanceof MySqlSelectQueryBlock)) {
              // it could be SQLUnionQuery
@@ -70,12 +68,18 @@ public class OrdinalRewriterRule implements RewriteRule<SQLQueryExpr> {
             return false;
         }
 
-        // we cannot clone SQLSelectItem, so we need similar objects to assign to GroupBy and OrderBy items
-        sqlExprGroupCopy = generateAST();
-        sqlExprOrderCopy = generateAST();
+        return true;
+    }
 
-        collectOrdinalAliases(sqlExprGroupCopy, groupOrdinalToExpr);
-        collectOrdinalAliases(sqlExprOrderCopy, orderOrdinalToExpr);
+    @Override
+    public void rewrite(SQLQueryExpr root) throws SQLFeatureNotSupportedException {
+
+        // we cannot clone SQLSelectItem, so we need similar objects to assign to GroupBy and OrderBy items
+        sqlExprGroupCopy = toSqlExpr();
+        sqlExprOrderCopy = toSqlExpr();
+
+        mapOrdinalIndexesToSelectItems(sqlExprGroupCopy, groupOrdinalToExpr);
+        mapOrdinalIndexesToSelectItems(sqlExprOrderCopy, orderOrdinalToExpr);
 
         if (groupOrdinalToExpr.isEmpty()) {
             // orderOrdinalToExpr and groupOrdinalToExpr should be identical
@@ -91,53 +95,9 @@ public class OrdinalRewriterRule implements RewriteRule<SQLQueryExpr> {
             .build();
 
         ruleExecutor.executeOn(sqlExprGroupCopy);
-        ruleExecutor.executeOn(sqlExprGroupCopy);
-        return true;
-    }
+        ruleExecutor.executeOn(sqlExprOrderCopy);
 
-    @Override
-    public void rewrite(SQLQueryExpr root) {
         changeOrdinalAliasInGroupAndOrderBy(root);
-    }
-
-    private boolean hasGroupByWithOrdinals(MySqlSelectQueryBlock query) {
-        if (query.getGroupBy() == null) {
-            return false;
-        } else if (query.getGroupBy().getItems().isEmpty()){
-            return false;
-        }
-
-        return query.getGroupBy().getItems().stream().anyMatch(x ->
-            x instanceof MySqlSelectGroupByExpr && ((MySqlSelectGroupByExpr) x).getExpr() instanceof SQLIntegerExpr
-        );
-    }
-
-    private boolean hasOrderByWithOrdinals(MySqlSelectQueryBlock query) {
-        if (query.getOrderBy() == null) {
-            return false;
-        } else if (query.getOrderBy().getItems().isEmpty()){
-            return false;
-        }
-
-        return query.getOrderBy().getItems().stream().anyMatch(x ->
-            x.getExpr() instanceof SQLIntegerExpr
-            || (
-                x.getExpr() instanceof SQLBinaryOpExpr
-                && ((SQLBinaryOpExpr) x.getExpr()).getLeft() instanceof SQLIntegerExpr
-            )
-        );
-    }
-
-    private void collectOrdinalAliases(SQLQueryExpr root, Map<Integer, SQLExpr> map) {
-        root.accept(new MySqlASTVisitorAdapter() {
-            private Integer count = 0;
-
-            @Override
-            public boolean visit(SQLSelectItem select) {
-                map.put(++count, select.getExpr());
-                return false;
-            }
-        });
     }
 
     private void changeOrdinalAliasInGroupAndOrderBy(SQLQueryExpr root) {
@@ -170,8 +130,8 @@ public class OrdinalRewriterRule implements RewriteRule<SQLQueryExpr> {
                     checkInvalidOrdinal(newExpr, ordinalValue, orderException);
                     orderByItem.setExpr(newExpr);
                     newExpr.setParent(orderByItem);
-                }else if (expr instanceof SQLBinaryOpExpr
-                          && ((SQLBinaryOpExpr) expr).getLeft() instanceof SQLIntegerExpr) {
+                } else if (expr instanceof SQLBinaryOpExpr
+                    && ((SQLBinaryOpExpr) expr).getLeft() instanceof SQLIntegerExpr) {
                     // support ORDER BY IS NULL/NOT NULL
                     SQLBinaryOpExpr binaryOpExpr = (SQLBinaryOpExpr) expr;
                     SQLIntegerExpr integerExpr = (SQLIntegerExpr) binaryOpExpr.getLeft();
@@ -188,18 +148,59 @@ public class OrdinalRewriterRule implements RewriteRule<SQLQueryExpr> {
         });
     }
 
-    private SQLQueryExpr generateAST() {
-        return (SQLQueryExpr) toSqlExpr(sql);
+    private void mapOrdinalIndexesToSelectItems(SQLQueryExpr root, Map<Integer, SQLExpr> map) {
+        root.accept(new MySqlASTVisitorAdapter() {
+            private Integer count = 0;
+
+            @Override
+            public boolean visit(SQLSelectItem select) {
+                map.put(++count, select.getExpr());
+                return false;
+            }
+        });
     }
 
-    private static SQLExpr toSqlExpr(String sql) {
+    private boolean hasGroupByWithOrdinals(MySqlSelectQueryBlock query) {
+        if (query.getGroupBy() == null) {
+            return false;
+        } else if (query.getGroupBy().getItems().isEmpty()){
+            return false;
+        }
+
+        return query.getGroupBy().getItems().stream().anyMatch(x ->
+            x instanceof MySqlSelectGroupByExpr && ((MySqlSelectGroupByExpr) x).getExpr() instanceof SQLIntegerExpr
+        );
+    }
+
+    private boolean hasOrderByWithOrdinals(MySqlSelectQueryBlock query) {
+        if (query.getOrderBy() == null) {
+            return false;
+        } else if (query.getOrderBy().getItems().isEmpty()){
+            return false;
+        }
+
+        /**
+         * The second condition checks valid AST that meets ORDER BY IS NULL/NOT NULL condition
+         *
+         *            SQLSelectOrderByItem
+         *                      |
+         *             SQLBinaryOpExpr (Is || IsNot)
+         *                    /  \
+         *    SQLIdentifierExpr  SQLNullExpr
+         */
+        return query.getOrderBy().getItems().stream().anyMatch(x ->
+            x.getExpr() instanceof SQLIntegerExpr
+            || (
+                x.getExpr() instanceof SQLBinaryOpExpr
+                && ((SQLBinaryOpExpr) x.getExpr()).getLeft() instanceof SQLIntegerExpr
+            )
+        );
+    }
+
+    private SQLQueryExpr toSqlExpr() {
         SQLExprParser parser = new ElasticSqlExprParser(sql);
         SQLExpr expr = parser.expr();
-
-        if (parser.getLexer().token() != Token.EOF) {
-            throw new ParserException("Illegal SQL expression : " + sql);
-        }
-        return expr;
+        return (SQLQueryExpr) expr;
     }
 
     private void checkInvalidOrdinal(SQLExpr expr, Integer ordinal, String exception){
