affaedf534f9fd313f949ab90e71344af4cb2b27
chloe-zh <fizhang@amazon>
2019-10-23 13:02:52 -0700

    Deleted the visit SQLProperty rewritten method from the UnquoteIdentifierRule, and modified the ElasticSqlExprParser to support a larger coverage of parsing quoted identifier cases

--- src/main/java/com/amazon/opendistroforelasticsearch/sql/rewriter/identifier/UnquoteIdentifierRule.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/sql/rewriter/identifier/UnquoteIdentifierRule.java
@@ -16,13 +16,11 @@
 package com.amazon.opendistroforelasticsearch.sql.rewriter.identifier;
 
 import com.alibaba.druid.sql.ast.expr.SQLIdentifierExpr;
-import com.alibaba.druid.sql.ast.expr.SQLPropertyExpr;
 import com.alibaba.druid.sql.ast.expr.SQLQueryExpr;
 import com.alibaba.druid.sql.ast.statement.SQLExprTableSource;
 import com.alibaba.druid.sql.ast.statement.SQLSelectItem;
 import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlASTVisitorAdapter;
 import com.amazon.opendistroforelasticsearch.sql.rewriter.RewriteRule;
-import com.amazon.opendistroforelasticsearch.sql.utils.StringUtils;
 
 import static com.amazon.opendistroforelasticsearch.sql.utils.StringUtils.unquoteFullColumn;
 import static com.amazon.opendistroforelasticsearch.sql.utils.StringUtils.unquoteSingleField;
@@ -55,32 +53,6 @@ public class UnquoteIdentifierRule extends MySqlASTVisitorAdapter implements Rew
         }
         selectItem.setAlias(unquoteSingleField(selectItem.getAlias(), "`"));
         return true;
-    }
-
-    /**
-     *
-     * This method is to adjust the AST in the cases where the alias of index is quoted
-     * (e.g. SELECT `b`.lastname FROM bank AS `b`).
-     *
-     * In this case, the druid parser constructs a SQLPropertyExpr for the field "`b`.lastname", with owner of a
-     * SQLIdentifierExpr "`b`" and name of "lastname".
-     *
-     * This method prevent the visitor from visitin the SQLPropertyExpr in this case,
-     * and corrects AST with a SQLSelectItem object to have SQLIdentifier of "b.lastname".
-     *
-     * Used in the case where alias of index and the field name are both quoted
-     * (e.g. SELECT `b`.`lastname` FROM bank AS `b`).
-     */
-    @Override
-    public boolean visit(SQLPropertyExpr propertyExpr) {
-        String fieldName = ((SQLIdentifierExpr) propertyExpr.getOwner()).getName();
-        if (!StringUtils.isQuoted(fieldName, "`")) {
-            return true;
-        }
-        String correctedIdentifier = unquoteSingleField(fieldName) + "." + unquoteSingleField(propertyExpr.getName());
-        SQLSelectItem selectItem = (SQLSelectItem) propertyExpr.getParent();
-        selectItem.setExpr(new SQLIdentifierExpr(correctedIdentifier));
-        return false;
     }
 
     @Override
