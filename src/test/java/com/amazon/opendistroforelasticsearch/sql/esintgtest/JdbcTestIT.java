271a6ee9f576e4b4742711e80a13341d3e70ef2c
chloe-zh <fizhang@amazon>
2019-11-20 12:08:33 -0800

    Updated

--- src/test/java/com/amazon/opendistroforelasticsearch/sql/esintgtest/JdbcTestIT.java
+++ src/test/java/com/amazon/opendistroforelasticsearch/sql/esintgtest/JdbcTestIT.java
@@ -145,11 +145,6 @@ public class JdbcTestIT extends SQLIntegTestCase {
                         + " ORDER BY substring(lastname, 1, 2)", "jdbc"),
                 containsString("\"name\": \"substring(lastname, 1, 2)\"")
         );
-        assertThat(
-                executeQuery("SELECT log(balance) FROM " + TestsConstants.TEST_INDEX_ACCOUNT
-                        + " ORDER BY log(balance)", "jdbc"),
-                containsString("\"name\": \"log(balance)\"")
-        );
     }
 
     @Test
@@ -158,11 +153,6 @@ public class JdbcTestIT extends SQLIntegTestCase {
                 executeQuery("SELECT substring(lastname, 1, 2) AS substring FROM "
                         + TestsConstants.TEST_INDEX_ACCOUNT + " ORDER BY substring", "jdbc"),
                 containsString("\"name\": \"substring\"")
-        );
-        assertThat(
-                executeQuery("SELECT log(balance) AS log FROM " + TestsConstants.TEST_INDEX_ACCOUNT
-                        + " ORDER BY log", "jdbc"),
-                containsString("\"name\": \"log\"")
         );
     }
 }
