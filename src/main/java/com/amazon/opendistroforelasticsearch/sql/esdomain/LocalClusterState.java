6f31ca79ba9018701d291bf845198468543bef7b
Jordan Wilson <jordanw@bitquilltech.com>
2020-02-24 12:06:05 -0800

    remove unused import

--- src/main/java/com/amazon/opendistroforelasticsearch/sql/esdomain/LocalClusterState.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/sql/esdomain/LocalClusterState.java
@@ -31,7 +31,6 @@ import org.elasticsearch.index.IndexNotFoundException;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
