c7aaf016d7cc28a84dc41d088de9bf3300b3ed7a
George Chen <qchea@amazon.com>
2020-03-18 10:51:51 -0500

    STY: unused import

--- src/main/java/com/amazon/opendistroforelasticsearch/sql/query/planner/converter/SQLToOperatorConverter.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/sql/query/planner/converter/SQLToOperatorConverter.java
@@ -32,7 +32,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.Client;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
