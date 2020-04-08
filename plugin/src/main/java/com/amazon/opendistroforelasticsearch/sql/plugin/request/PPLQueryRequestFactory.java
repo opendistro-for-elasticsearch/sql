13f8b85ae00269d2fd673ac330a0d336d2e5627a
Peng Huo <penghuo@gmail.com>
2020-04-08 15:41:21 -0700

    [PPL] add basic parser support (#415)
    * add basic parser support
    
    * Reduce the permission by removing java.security.AllPermission

---
+++ plugin/src/main/java/com/amazon/opendistroforelasticsearch/sql/plugin/request/PPLQueryRequestFactory.java
@@ -0,0 +1,58 @@
+/*
+ *   Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.sql.plugin.request;
+
+import com.amazon.opendistroforelasticsearch.sql.ppl.domain.PPLQueryRequest;
+import org.elasticsearch.rest.RestRequest;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PPLQueryRequestFactory {
+    private static final String PPL_URL_PARAM_KEY = "ppl";
+    private static final String PPL_FIELD_NAME = "query";
+
+    public static PPLQueryRequest getPPLRequest(RestRequest request) {
+        switch (request.method()) {
+            case GET:
+                return parsePPLRequestFromUrl(request);
+            case POST:
+                return parsePPLRequestFromPayload(request);
+            default:
+                throw new IllegalArgumentException("ES PPL doesn't supported HTTP " + request.method().name());
+        }
+    }
+
+    private static PPLQueryRequest parsePPLRequestFromUrl(RestRequest restRequest) {
+        String ppl;
+
+        ppl = restRequest.param(PPL_URL_PARAM_KEY);
+        if (ppl == null) {
+            throw new IllegalArgumentException("Cannot find ppl parameter from the URL");
+        }
+        return new PPLQueryRequest(ppl, null);
+    }
+
+    private static PPLQueryRequest parsePPLRequestFromPayload(RestRequest restRequest) {
+        String content = restRequest.content().utf8ToString();
+        JSONObject jsonContent;
+        try {
+            jsonContent = new JSONObject(content);
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("Failed to parse request payload", e);
+        }
+        return new PPLQueryRequest(jsonContent.getString(PPL_FIELD_NAME), jsonContent);
+    }
+}
---
+++ ppl/src/test/java/com/amazon/opendistroforelasticsearch/sql/ppl/antlr/PPLSyntaxParserTest.java
@@ -0,0 +1,54 @@
+/*
+ *   Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.sql.ppl.antlr;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertNotEquals;
+
+public class PPLSyntaxParserTest {
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void testSearchCommandShouldPass() {
+        ParseTree tree = new PPLSyntaxParser().analyzeSyntax("search source=t a=1 b=2");
+        assertNotEquals(null, tree);
+    }
+
+    @Test
+    public void testSearchCommandIgnoreSearchKeywordShouldPass() {
+        ParseTree tree = new PPLSyntaxParser().analyzeSyntax("source=t a=1 b=2");
+        assertNotEquals(null, tree);
+    }
+
+    @Test
+    public void testSearchFieldsCommandShouldPass() {
+        ParseTree tree = new PPLSyntaxParser().analyzeSyntax("search source=t a=1 b=2 | fields a,b");
+        assertNotEquals(null, tree);
+    }
+
+    @Test
+    public void testSearchCommandWithoutSourceShouldFail() {
+        exceptionRule.expect(RuntimeException.class);
+        exceptionRule.expectMessage("Failed to parse query due to offending symbol");
+
+        new PPLSyntaxParser().analyzeSyntax("search a=1");
+    }
+}
