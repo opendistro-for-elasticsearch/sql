878aefaec1e33f4a373ee6bd3a7b7357822bb57c
chloe-zh <fizhang@amazon.com>
2020-05-27 00:33:03 -0700

    Added PPL search command integration test

---
+++ integ-test/src/test/java/com/amazon/opendistroforelasticsearch/sql/ppl/SearchCommandIT.java
@@ -0,0 +1,60 @@
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.sql.ppl;
+
+import java.io.IOException;
+import org.elasticsearch.client.ResponseException;
+import org.junit.Test;
+import static com.amazon.opendistroforelasticsearch.sql.esintgtest.TestsConstants.TEST_INDEX_BANK;
+
+public class SearchCommandIT extends PPLIntegTestCase {
+
+    @Override
+    public void init() throws IOException {
+        loadIndex(Index.BANK);
+    }
+
+    @Test
+    public void testSearchAllFields() throws IOException {
+        String result = executeQuery(String.format("search source=%s", TEST_INDEX_BANK));
+        assertTrue(result.contains("\"total\": 7"));
+    }
+
+    @Test
+    public void testSearchCommandWithoutSearchKeyword() throws IOException {
+        assertEquals(
+                executeQuery(String.format("search source=%s", TEST_INDEX_BANK)),
+                executeQuery(String.format("source=%s", TEST_INDEX_BANK))
+        );
+    }
+
+    @Test
+    public void testSearchCommandWithLogicalExpression() throws IOException {
+        String result = executeQuery(String.format("search source=%s firstname='Hattie'", TEST_INDEX_BANK));
+        assertTrue(result.contains("\"Hattie\""));
+    }
+
+    @Test
+    public void searchCommandWithoutSourceShouldFailToParse() throws IOException {
+        try {
+            executeQuery("search firstname='Hattie'");
+            fail();
+        } catch (ResponseException e) {
+            assertTrue(e.getMessage().contains("RuntimeException"));
+            assertTrue(e.getMessage().contains("Failed to parse query due to offending symbol"));
+        }
+    }
+}
