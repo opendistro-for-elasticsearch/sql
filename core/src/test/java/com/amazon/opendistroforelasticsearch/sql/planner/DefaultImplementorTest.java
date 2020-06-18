6b0197cf36c2ac4a6d48b51a3c62b08c0381f6d1
Dai <daichen@amazon.com>
2020-06-17 22:04:34 -0700

    Add more UT

---
+++ core/src/test/java/com/amazon/opendistroforelasticsearch/sql/planner/DefaultImplementorTest.java
@@ -0,0 +1,125 @@
+/*
+ *    Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License").
+ *    You may not use this file except in compliance with the License.
+ *    A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    or in the "license" file accompanying this file. This file is distributed
+ *    on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *    express or implied. See the License for the specific language governing
+ *    permissions and limitations under the License.
+ *
+ */
+
+package com.amazon.opendistroforelasticsearch.sql.planner;
+
+import static com.amazon.opendistroforelasticsearch.sql.expression.DSL.literal;
+import static com.amazon.opendistroforelasticsearch.sql.expression.DSL.ref;
+import static com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL.aggregation;
+import static com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL.eval;
+import static com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL.filter;
+import static com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL.project;
+import static com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL.remove;
+import static com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL.rename;
+import static com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL.sort;
+import static com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL.values;
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.amazon.opendistroforelasticsearch.sql.ast.tree.Sort;
+import com.amazon.opendistroforelasticsearch.sql.data.model.ExprBooleanValue;
+import com.amazon.opendistroforelasticsearch.sql.data.model.ExprType;
+import com.amazon.opendistroforelasticsearch.sql.expression.Expression;
+import com.amazon.opendistroforelasticsearch.sql.expression.ReferenceExpression;
+import com.amazon.opendistroforelasticsearch.sql.expression.aggregation.Aggregator;
+import com.amazon.opendistroforelasticsearch.sql.expression.aggregation.AvgAggregator;
+import com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlan;
+import com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalPlanDSL;
+import com.amazon.opendistroforelasticsearch.sql.planner.logical.LogicalRelation;
+import com.amazon.opendistroforelasticsearch.sql.planner.physical.PhysicalPlan;
+import com.amazon.opendistroforelasticsearch.sql.planner.physical.PhysicalPlanDSL;
+import com.google.common.collect.ImmutableMap;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Test;
+
+class DefaultImplementorTest {
+
+  private final DefaultImplementor<Object> implementor = new DefaultImplementor<>();
+
+  @Test
+  public void visitShouldReturnDefaultPhysicalOperator() {
+    String indexName = "test";
+    ReferenceExpression include = ref("age");
+    ReferenceExpression exclude = ref("name");
+    ReferenceExpression dedupeField = ref("name");
+    Expression filterExpr = literal(ExprBooleanValue.ofTrue());
+    List<Expression> groupByExprs = Arrays.asList(ref("age"));
+    List<Aggregator> aggregators = Arrays.asList(new AvgAggregator(groupByExprs, ExprType.DOUBLE));
+    Map<ReferenceExpression, ReferenceExpression> mappings =
+        ImmutableMap.of(ref("name"), ref("lastname"));
+    Pair<ReferenceExpression, Expression> newEvalField =
+        ImmutablePair.of(ref("name1"), ref("name"));
+    Integer sortCount = 100;
+    Pair<Sort.SortOption, Expression> sortField =
+        ImmutablePair.of(Sort.SortOption.PPL_ASC, ref("name1"));
+
+    LogicalPlan plan =
+        project(
+            LogicalPlanDSL.dedupe(
+                sort(
+                    eval(
+                        remove(
+                            rename(
+                                aggregation(
+                                    filter(values(emptyList()), filterExpr),
+                                    aggregators,
+                                    groupByExprs),
+                                mappings),
+                            exclude),
+                        newEvalField),
+                    sortCount,
+                    sortField),
+                dedupeField),
+            include);
+
+    PhysicalPlan actual = plan.accept(implementor, null);
+
+    assertEquals(
+        PhysicalPlanDSL.project(
+            PhysicalPlanDSL.dedupe(
+                PhysicalPlanDSL.sort(
+                    PhysicalPlanDSL.eval(
+                        PhysicalPlanDSL.remove(
+                            PhysicalPlanDSL.rename(
+                                PhysicalPlanDSL.agg(
+                                    PhysicalPlanDSL.filter(
+                                        PhysicalPlanDSL.values(emptyList()),
+                                        filterExpr),
+                                    aggregators,
+                                    groupByExprs),
+                                mappings),
+                            exclude),
+                        newEvalField),
+                    sortCount,
+                    sortField),
+                dedupeField),
+            include),
+        actual);
+
+  }
+
+  @Test
+  public void visitRelationShouldThrowException() {
+    assertThrows(UnsupportedOperationException.class,
+        () -> new LogicalRelation("test").accept(implementor, null));
+  }
+
+}
