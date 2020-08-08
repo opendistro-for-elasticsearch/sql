432b81b36f0fdfefe7edf6d39fb9a58e4f276fac
Jordan Wilson <jordanw@bitquilltech.com>
2020-08-08 00:01:38 -0700

    workflow & ci script updates

--- sql-odbc/scripts/prepare_ci_output.ps1
+++ sql-odbc/scripts/prepare_ci_output.ps1
@@ -5,7 +5,7 @@ $ODBC_BUILD_PATH = $args[2]
 Write-Host $args
 
 # Create staging directories for CI artifacts
-$CI_OUTPUT_PATH = .\ci-output
+$CI_OUTPUT_PATH = ".\ci-output"
 New-Item -Path $CI_OUTPUT_PATH -ItemType Directory -Force | Out-Null
 New-Item -Path $CI_OUTPUT_PATH\build -ItemType Directory -Force | Out-Null
 New-Item -Path $CI_OUTPUT_PATH\installer -ItemType Directory -Force | Out-Null
