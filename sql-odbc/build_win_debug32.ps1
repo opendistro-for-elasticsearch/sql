4e797e5c7966e89d24c8383bdbb8560255e0cad4
Jordan Wilson <37088125+jordanw-bq@users.noreply.github.com>
2020-08-18 12:58:44 -0700

    ODBC: improve Windows build process (#661)
    * improve Windows build process
    
    - combine build code into one script for easier edits
    - introduced UNITY build for AWS SDK
    
    * update CI scripts
    
    * workflow updates
    
    * more workflow updates
    
    * workflow updates
    
    * workflow updates
    
    * update WinArch for VLD dll
    
    * print build_installer args
    
    * use Windows path separator & print CI output args for debugging
    
    * use Powershell envar syntax
    
    * workflow & ci script updates
    
    * update cmake path for 32-bit install
    
    * fix uplaod step path names
    
    * adding Env prefix for artifact steps
    
    * hardcode upload step paths
    
    * add link to custom test dataset
    
    * copy build script upgrades to release workflow

--- sql-odbc/build_win_debug32.ps1
+++ sql-odbc/build_win_debug32.ps1
@@ -1,27 +1,2 @@
-# Build AWS SDK
-$BITNESS=32
-
-# Compare Bitness for 32
-# $ARCH="Win32"
-
-mkdir sdk-build${BITNESS}
-cd sdk-build${BITNESS}
-
-git clone "https://github.com/aws/aws-sdk-cpp.git"
-
-$prefix_path = (pwd).path
-cmake .\aws-sdk-cpp -A Win32 -D CMAKE_INSTALL_PREFIX=${prefix_path}\AWSSDK\ -D CMAKE_BUILD_TYPE=Debug -D BUILD_ONLY="core" -D CUSTOM_MEMORY_MANAGEMENT="OFF" -D ENABLE_RTTI="OFF" -D ENABLE_TESTING="OFF"
-
-msbuild ALL_BUILD.vcxproj /m /p:Configuration=Debug
-msbuild INSTALL.vcxproj /m /p:Configuration=Debug
-
-cd ..
-
-# # Configure Project
-cmake -S src -B cmake-build${BITNESS} -A Win32 -D CMAKE_INSTALL_PREFIX=sdk-build${BITNESS}\AWSSDK\ -D BUILD_WITH_TESTS=ON
-
-# # Build Project
-cmake --build .\cmake-build${BITNESS} --config Debug --parallel 4
-
-cp .\sdk-build32\bin\Debug\* .\bin32\Debug
-cp .\cmake-build32\bin\Debug\* .\bin32\Debug
+$WORKING_DIR = (Get-Location).Path
+.\scripts\build_windows.ps1 $WORKING_DIR Debug 32
