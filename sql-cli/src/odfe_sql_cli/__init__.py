5631a8eec9a58e741f92949f96150bfaf8ed2e87
Joshua Li <joshuali925@gmail.com>
2020-09-03 12:42:22 -0700

    bump to 7.9.1, odfe 1.10.1

--- sql-cli/src/odfe_sql_cli/__init__.py
+++ sql-cli/src/odfe_sql_cli/__init__.py
@@ -12,4 +12,4 @@ on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 express or implied. See the License for the specific language governing
 permissions and limitations under the License.
 """
-__version__ = "1.10.0.0"
+__version__ = "1.10.1.0"
