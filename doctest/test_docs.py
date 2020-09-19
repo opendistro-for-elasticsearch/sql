336488385b34774403e8cc96a73c5aa5dcea08f2
lyndonb-bq <lyndonb@bitquilltech.com>
2020-09-19 01:47:15 -0700

    [1] Trying again.

--- doctest/test_docs.py
+++ doctest/test_docs.py
@@ -45,8 +45,11 @@ class DocTestConnection(ESConnection):
 
     def process(self, statement):
         data = self.execute_query(statement, use_console=False)
+        click.echo("Joined returned: " + str(data))
         output = self.formatter.format_output(data)
+        click.echo("From formatter: " + str(output))
         output = "\n".join(output)
+        click.echo("\\n joined: " + str(output))
 
         click.echo(output)
 
