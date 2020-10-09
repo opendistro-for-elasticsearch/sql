7793ab34a22566c9d643faf8d3dedfd6cd7ee97f
Dai <daichen@amazon.com>
2020-10-09 14:59:11 -0700

    Skip pattern match if not a regex pattern

--- legacy/src/main/java/com/amazon/opendistroforelasticsearch/sql/legacy/executor/format/DescribeResultSet.java
+++ legacy/src/main/java/com/amazon/opendistroforelasticsearch/sql/legacy/executor/format/DescribeResultSet.java
@@ -95,8 +95,11 @@ public class DescribeResultSet extends ResultSet {
         for (ObjectObjectCursor<String, ImmutableOpenMap<String, MappingMetadata>> indexCursor : indexMappings) {
             String index = indexCursor.key;
 
-            // Check to see if index matches given pattern
-            if (matchesPattern(index, statement.getIndexPattern())) {
+            // Check to see if index matches given pattern. To support describe alias and avoid mismatch
+            // between actual index name and pattern (alias), the temp fix is to skip match logic if
+            // no regex character found in the pattern given.
+            String indexPattern = statement.getIndexPattern();
+            if (isNotRegexPattern(indexPattern) || matchesPattern(index, indexPattern)) {
                 ImmutableOpenMap<String, MappingMetadata> typeMapping = indexCursor.value;
                 // Assuming ES 6.x, iterate through the only type of the index to get mapping data
                 for (ObjectObjectCursor<String, MappingMetadata> typeCursor : typeMapping) {
@@ -180,5 +183,9 @@ public class DescribeResultSet extends ResultSet {
         }
 
         return currPath + "." + field;
+    }
+
+    private boolean isNotRegexPattern(String pattern) {
+        return !pattern.contains(".") && !pattern.contains("*");
     }
 }
