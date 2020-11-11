8c84ae16caf72413d221678e37bdb371b8592b28
chloe-zh <fizhang@amazon.com>
2020-11-11 15:33:24 -0800

    added unit test

--- core/src/main/java/com/amazon/opendistroforelasticsearch/sql/ast/dsl/AstDSL.java
+++ core/src/main/java/com/amazon/opendistroforelasticsearch/sql/ast/dsl/AstDSL.java
@@ -46,6 +46,7 @@ import com.amazon.opendistroforelasticsearch.sql.ast.tree.Project;
 import com.amazon.opendistroforelasticsearch.sql.ast.tree.RareTopN;
 import com.amazon.opendistroforelasticsearch.sql.ast.tree.RareTopN.CommandType;
 import com.amazon.opendistroforelasticsearch.sql.ast.tree.Relation;
+import com.amazon.opendistroforelasticsearch.sql.ast.tree.RelationSubquery;
 import com.amazon.opendistroforelasticsearch.sql.ast.tree.Rename;
 import com.amazon.opendistroforelasticsearch.sql.ast.tree.Sort;
 import com.amazon.opendistroforelasticsearch.sql.ast.tree.UnresolvedPlan;
@@ -120,6 +121,10 @@ public class AstDSL {
 
   public static UnresolvedExpression unresolvedAttr(String attr) {
     return new UnresolvedAttribute(attr);
+  }
+
+  public static UnresolvedPlan relationSubquery(UnresolvedPlan subquery, String subqueryAlias) {
+    return new RelationSubquery(subquery, subqueryAlias);
   }
 
   private static Literal literal(Object value, DataType type) {
