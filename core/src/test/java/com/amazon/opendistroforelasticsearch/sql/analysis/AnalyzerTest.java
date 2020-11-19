57fc196b60d08b850d1ca0a81f78af7d40385b06
Dai <daichen@amazon.com>
2020-11-19 11:53:03 -0800

    Add comparison test

--- core/src/test/java/com/amazon/opendistroforelasticsearch/sql/analysis/AnalyzerTest.java
+++ core/src/test/java/com/amazon/opendistroforelasticsearch/sql/analysis/AnalyzerTest.java
@@ -344,21 +344,20 @@ class AnalyzerTest extends AnalyzerTestBase {
                 new SortOption(SortOrder.DESC, NullOrder.NULL_LAST))
             .build();
 
-    argOptions.forEach((args, expectOption) -> {
-      assertAnalyzeEqual(
-          LogicalPlanDSL.project(
-              LogicalPlanDSL.sort(
-                  LogicalPlanDSL.relation("test"),
-                  0,
-                  Pair.of(expectOption, DSL.ref("integer_value", INTEGER))),
-              DSL.named("string_value", DSL.ref("string_value", STRING))),
-          AstDSL.project(
-              AstDSL.sort(
-                  AstDSL.relation("test"),
-                  ImmutableList.of(argument("count", intLiteral(0))),
-                  field(qualifiedName("integer_value"), args)),
-              AstDSL.alias("string_value", qualifiedName("string_value"))));
-    });
+    argOptions.forEach((args, expectOption) ->
+        assertAnalyzeEqual(
+            LogicalPlanDSL.project(
+                LogicalPlanDSL.sort(
+                    LogicalPlanDSL.relation("test"),
+                    0,
+                    Pair.of(expectOption, DSL.ref("integer_value", INTEGER))),
+                DSL.named("string_value", DSL.ref("string_value", STRING))),
+            AstDSL.project(
+                AstDSL.sort(
+                    AstDSL.relation("test"),
+                    ImmutableList.of(argument("count", intLiteral(0))),
+                    field(qualifiedName("integer_value"), args)),
+                AstDSL.alias("string_value", qualifiedName("string_value")))));
   }
 
   @SuppressWarnings("unchecked")
