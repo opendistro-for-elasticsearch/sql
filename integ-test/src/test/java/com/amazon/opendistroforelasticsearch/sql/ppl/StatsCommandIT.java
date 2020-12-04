eef0684f4292ec9d019079ccb3128c8e566b0966
Peng Huo <penghuo@gmail.com>
2020-12-04 08:32:15 -0800

    Extend the PPL identifier defintion (#888)
    * Extend the PPL identifier defintion
    
    * update
    
    * fix bug

--- integ-test/src/test/java/com/amazon/opendistroforelasticsearch/sql/ppl/StatsCommandIT.java
+++ integ-test/src/test/java/com/amazon/opendistroforelasticsearch/sql/ppl/StatsCommandIT.java
@@ -79,7 +79,8 @@ public class StatsCommandIT extends PPLIntegTestCase {
   @Test
   public void testStatsNested() throws IOException {
     JSONObject response =
-        executeQuery(String.format("source=%s | stats avg(abs(age)*2) as AGE", TEST_INDEX_ACCOUNT));
+        executeQuery(String.format("source=%s | stats avg(abs(age) * 2) as AGE",
+            TEST_INDEX_ACCOUNT));
     verifySchema(response, schema("AGE", null, "double"));
     verifyDataRows(response, rows(60.342));
   }
@@ -87,9 +88,9 @@ public class StatsCommandIT extends PPLIntegTestCase {
   @Test
   public void testStatsNestedDoubleValue() throws IOException {
     JSONObject response =
-        executeQuery(String.format("source=%s | stats avg(abs(age)*2.0)",
+        executeQuery(String.format("source=%s | stats avg(abs(age) * 2.0)",
             TEST_INDEX_ACCOUNT));
-    verifySchema(response, schema("avg(abs(age)*2.0)", null, "double"));
+    verifySchema(response, schema("avg(abs(age) * 2.0)", null, "double"));
     verifyDataRows(response, rows(60.342));
   }
 
