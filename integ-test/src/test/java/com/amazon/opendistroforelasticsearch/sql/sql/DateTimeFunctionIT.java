0eebbf9fcfddfb2512521185dc56349e667d3316
Rupal Mahajan <>
2020-12-07 15:01:40 -0800

    add manual IT

--- integ-test/src/test/java/com/amazon/opendistroforelasticsearch/sql/sql/DateTimeFunctionIT.java
+++ integ-test/src/test/java/com/amazon/opendistroforelasticsearch/sql/sql/DateTimeFunctionIT.java
@@ -20,8 +20,10 @@ import static com.amazon.opendistroforelasticsearch.sql.util.MatcherUtils.rows;
 import static com.amazon.opendistroforelasticsearch.sql.util.MatcherUtils.schema;
 import static com.amazon.opendistroforelasticsearch.sql.util.MatcherUtils.verifyDataRows;
 import static com.amazon.opendistroforelasticsearch.sql.util.MatcherUtils.verifySchema;
+import static com.amazon.opendistroforelasticsearch.sql.legacy.TestsConstants.TEST_INDEX_BANK;
 import static com.amazon.opendistroforelasticsearch.sql.util.TestUtils.getResponseBody;
 
+import com.amazon.opendistroforelasticsearch.sql.ast.expression.In;
 import com.amazon.opendistroforelasticsearch.sql.common.utils.StringUtils;
 import com.amazon.opendistroforelasticsearch.sql.legacy.SQLIntegTestCase;
 import com.amazon.opendistroforelasticsearch.sql.util.TestUtils;
@@ -39,6 +41,23 @@ public class DateTimeFunctionIT extends SQLIntegTestCase {
   public void init() throws Exception {
     super.init();
     TestUtils.enableNewQueryEngine(client());
+    loadIndex(Index.BANK);
+  }
+
+  @Test
+  public void testDateInGroupBy() throws IOException{
+    JSONObject result =
+            executeQuery(String.format("SELECT DATE(birthdate) FROM %s GROUP BY DATE(birthdate)",TEST_INDEX_BANK) );
+    verifySchema(result,
+            schema("DATE(birthdate)", null, "date"));
+    verifyDataRows(result,
+            rows("2017-10-23"),
+            rows("2017-11-20"),
+            rows("2018-06-23"),
+            rows("2018-11-13"),
+            rows("2018-06-27"),
+            rows("2018-08-19"),
+            rows("2018-08-11"));
   }
 
   @Test
