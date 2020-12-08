8d37cba009d8e4b0fabda776c849f99f09224f30
chloe-zh <chloezh1102@gmail.com>
2020-12-08 11:43:27 -0800

    added IT for ppl

---
+++ integ-test/src/test/java/com/amazon/opendistroforelasticsearch/sql/ppl/CsvFormatIT.java
@@ -0,0 +1,43 @@
+package com.amazon.opendistroforelasticsearch.sql.ppl;
+
+import static com.amazon.opendistroforelasticsearch.sql.legacy.TestsConstants.TEST_INDEX_BANK_CSV_SANITIZE;
+
+import java.io.IOException;
+import java.util.Locale;
+import org.junit.Test;
+
+public class CsvFormatIT extends PPLIntegTestCase {
+
+  @Override
+  public void init() throws IOException {
+    loadIndex(Index.BANK_CSV_SANITIZE);
+  }
+
+  @Test
+  public void sanitizeTest() throws IOException {
+    String result = executeCsvQuery(
+        String.format(Locale.ROOT, "source=%s | fields firstname, lastname", TEST_INDEX_BANK_CSV_SANITIZE));
+    assertEquals(
+        "firstname,lastname\n"
+            + "'+Amber JOHnny,Duke Willmington+\n"
+            + "'-Hattie,Bond-\n"
+            + "'=Nanette,Bates=\n"
+            + "'@Dale,Adams@\n"
+            + "\",Elinor\",\"Ratliff,,,\"\n",
+        result);
+  }
+
+  @Test
+  public void escapeSanitizeTest() throws IOException {
+    String result = executeCsvQuery(
+        String.format(Locale.ROOT, "source=%s | fields firstname, lastname", TEST_INDEX_BANK_CSV_SANITIZE), true);
+    assertEquals(
+        "firstname,lastname\n"
+            + "+Amber JOHnny,Duke Willmington+\n"
+            + "-Hattie,Bond-\n"
+            + "=Nanette,Bates=\n"
+            + "@Dale,Adams@\n"
+            + ",Elinor,Ratliff,,,\n",
+        result);
+  }
+}
