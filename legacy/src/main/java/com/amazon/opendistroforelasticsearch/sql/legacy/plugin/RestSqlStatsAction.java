81c94aa8a1130728e9926271f76a52fa63bc4e40
chloe-zh <chloezh1102@gmail.com>
2020-12-09 17:08:52 -0800

    added responseParams override method to sql and ppl stats actions

--- legacy/src/main/java/com/amazon/opendistroforelasticsearch/sql/legacy/plugin/RestSqlStatsAction.java
+++ legacy/src/main/java/com/amazon/opendistroforelasticsearch/sql/legacy/plugin/RestSqlStatsAction.java
@@ -84,7 +84,7 @@ public class RestSqlStatsAction extends BaseRestHandler {
     @Override
     protected Set<String> responseParams() {
         Set<String> responseParams = new HashSet<>(super.responseParams());
-        responseParams.addAll(Arrays.asList("sql", "flat", "separator", "_score", "_type", "_id", "newLine", "format"));
+        responseParams.addAll(Arrays.asList("sql", "flat", "separator", "_score", "_type", "_id", "newLine", "format", "sanitize"));
         return responseParams;
     }
 
