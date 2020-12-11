84f6f844fbfa5f814435851cb135aeeacb764a4d
chloe-zh <chloezh1102@gmail.com>
2020-12-11 11:25:40 -0800

    addressed comments

--- protocol/src/main/java/com/amazon/opendistroforelasticsearch/sql/protocol/response/format/CsvResponseFormatter.java
+++ protocol/src/main/java/com/amazon/opendistroforelasticsearch/sql/protocol/response/format/CsvResponseFormatter.java
@@ -23,10 +23,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Singular;
 
 @RequiredArgsConstructor
 public class CsvResponseFormatter implements ResponseFormatter<QueryResult> {
@@ -42,12 +40,8 @@ public class CsvResponseFormatter implements ResponseFormatter<QueryResult> {
 
   @Override
   public String format(QueryResult response) {
-    CsvResult result = buildCsvResult(response);
-    String headers = String.join(INLINE_SEPARATOR, result.getHeaders());
-    ImmutableList.Builder<String> dataLines = new ImmutableList.Builder<>();
-    result.getData().forEach(line -> dataLines.add(String.join(INLINE_SEPARATOR, line)));
-    return String.join(
-        INTERLINE_SEPARATOR, headers, String.join(INTERLINE_SEPARATOR, dataLines.build()));
+    CsvResult result = new CsvResult(response, sanitize);
+    return result.getCsv();
   }
 
   @Override
@@ -55,41 +49,54 @@ public class CsvResponseFormatter implements ResponseFormatter<QueryResult> {
     return ErrorFormatter.prettyFormat(t);
   }
 
-  private CsvResult buildCsvResult(QueryResult response) {
-    CsvResult.Builder builder = CsvResult.builder();
-    ImmutableList.Builder<List<String>> dataLines = new ImmutableList.Builder<>();
 
-    response.columnNameTypes().forEach((column, type) -> builder.header(column));
-    response.iterator().forEachRemaining(row -> {
-      ImmutableList.Builder<String> line = new ImmutableList.Builder<>();
-      // replace null values with empty string
-      Arrays.asList(row).forEach(val -> line.add(val == null ? "" : val.toString()));
-      dataLines.add(line.build());
-    });
-    builder.data(dataLines.build());
-
-    CsvResult result = builder.build();
-    return sanitize ? result.sanitize() : result;
-  }
-
-  @Builder(builderClassName = "Builder")
+  /**
+   * Sanitize methods are migrated from legacy CSV result.
+   * To deal with special character in column name and to avoid csv injection.
+   * Sanitize both headers and data lines by:
+   *  1) First prepend single quote at the start if first char is sensitive (= - + @);
+   *  2) Second double quote entire cell if any comma is found.
+   */
   @Getter
+  @RequiredArgsConstructor
   static class CsvResult {
-    @Singular("header")
-    private List<String> headers;
-    private List<List<String>> data;
+    private final QueryResult response;
+    private final boolean sanitize;
 
-    /**
-     * Migrated from legacy CSV result.
-     * To deal with special character in column name and to avoid csv injection.
-     * Sanitize both headers and data lines by:
-     *  1) First prepend single quote if first char is sensitive (= - + @);
-     *  2) Second double quote entire cell if any comma found.
-     */
-    public CsvResult sanitize() {
-      headers = sanitizeHeaders(headers);
-      data = sanitizeData(data);
-      return this;
+    public String getCsv() {
+      List<String> headersAndData = new ArrayList<>();
+      headersAndData.add(getHeaderLine(response, sanitize));
+      headersAndData.addAll(getDataLines(response, sanitize));
+      return String.join(INTERLINE_SEPARATOR, headersAndData);
+    }
+
+    private String getHeaderLine(QueryResult response, boolean sanitize) {
+      List<String> headers = getHeaders(response, sanitize);
+      return String.join(INLINE_SEPARATOR, headers);
+    }
+
+    private List<String> getDataLines(QueryResult response, boolean sanitize) {
+      List<List<String>> data = getData(response, sanitize);
+      return data.stream().map(v -> String.join(INLINE_SEPARATOR, v)).collect(Collectors.toList());
+    }
+
+    private List<String> getHeaders(QueryResult response, boolean sanitize) {
+      ImmutableList.Builder<String> headers = ImmutableList.builder();
+      response.columnNameTypes().forEach((column, type) -> headers.add(column));
+      List<String> result = headers.build();
+      return sanitize ? sanitizeHeaders(result) : result;
+    }
+
+    private List<List<String>> getData(QueryResult response, boolean sanitize) {
+      ImmutableList.Builder<List<String>> dataLines = new ImmutableList.Builder<>();
+      response.iterator().forEachRemaining(row -> {
+        ImmutableList.Builder<String> line = new ImmutableList.Builder<>();
+        // replace null values with empty string
+        Arrays.asList(row).forEach(val -> line.add(val == null ? "" : val.toString()));
+        dataLines.add(line.build());
+      });
+      List<List<String>> result = dataLines.build();
+      return sanitize ? sanitizeData(result) : result;
     }
 
     /**
