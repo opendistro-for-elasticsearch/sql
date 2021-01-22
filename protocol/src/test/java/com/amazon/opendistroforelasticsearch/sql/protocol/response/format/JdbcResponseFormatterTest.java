f0297e347edc4f1bda99c5a2ed2c80e3d6a39d10
Dai <daichen@amazon.com>
2021-01-22 10:47:51 -0800

    Add more UT

--- protocol/src/test/java/com/amazon/opendistroforelasticsearch/sql/protocol/response/format/JdbcResponseFormatterTest.java
+++ protocol/src/test/java/com/amazon/opendistroforelasticsearch/sql/protocol/response/format/JdbcResponseFormatterTest.java
@@ -39,6 +39,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonParser;
 import java.util.Arrays;
+import org.elasticsearch.ElasticsearchException;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -151,6 +152,25 @@ class JdbcResponseFormatterTest {
             + "},"
             + "\"status\":503}",
         formatter.format(new IllegalStateException("Execution error"))
+    );
+  }
+
+  @Test
+  void format_server_error_response_due_to_elasticsearch() {
+    assertJsonEquals(
+        "{\"error\":"
+            + "{\""
+            + "type\":\"ElasticsearchException\","
+            + "\"reason\":\"Error occurred in Elasticsearch engine: all shards failed\","
+            + "\"details\":\"ElasticsearchException[all shards failed]; "
+            + "nested: IllegalStateException[Execution error];; "
+            + "java.lang.IllegalStateException: Execution error\\n"
+            + "For more details, please send request for Json format to see the raw response "
+            + "from elasticsearch engine.\""
+            + "},"
+            + "\"status\":503}",
+        formatter.format(new ElasticsearchException("all shards failed",
+            new IllegalStateException("Execution error")))
     );
   }
 
